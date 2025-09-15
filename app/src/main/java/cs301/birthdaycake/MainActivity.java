a9047513564745ed68d97caccc21e928cb51b4eb
JNuxoll <nuxoll28@up.edu>
2025-09-15 12:20:14 -0700

    finished lab 2

--- app/src/main/java/cs301/birthdaycake/MainActivity.java
+++ app/src/main/java/cs301/birthdaycake/MainActivity.java
@@ -2,6 +2,8 @@ package cs301.birthdaycake;
 
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -13,4 +15,10 @@ public class MainActivity extends AppCompatActivity {
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
         setContentView(R.layout.activity_main);
     }
+
+
+    public void goodbye(View button) {
+        Log.i("button","Goodbye");
+    }
+
 }
